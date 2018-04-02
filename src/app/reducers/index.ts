9011fe3e363da2bf8f21a04c185d11390a3aad6e
Danny Suarez <danny.suarez@tektonlabs.com>
2018-04-01 19:24:17 -0500

    added actions, models and reducers

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,13 @@
+import { Currency } from'./../models/currency';
+import * as fromAmount from'./amount';
+import * as fromCurrency from'./currency';
+
+export interface State {
+    amount: number;
+    currencies: Currency[];
+}
+
+export const reducers = {
+    amount: fromAmount.reducer,
+    currencies: fromCurrency.reducer
+};
